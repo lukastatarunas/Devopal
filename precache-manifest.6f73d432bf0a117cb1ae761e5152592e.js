820488617089db6e078e4a86980051f69a558265
lukas.tatarunas@shawk.eu <lukas.tatarunas@shawk.eu>
2019-10-14 15:14:27 +0300

    Updates

---
+++ precache-manifest.6f73d432bf0a117cb1ae761e5152592e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f426ce4a849ace00e39d2cd7746626a",
+    "url": "/Devopal/index.html"
+  },
+  {
+    "revision": "9307003545c4c70f10a4",
+    "url": "/Devopal/static/css/main.b8604304.chunk.css"
+  },
+  {
+    "revision": "9ebbac2385206a71dc44",
+    "url": "/Devopal/static/js/2.1f62e686.chunk.js"
+  },
+  {
+    "revision": "9307003545c4c70f10a4",
+    "url": "/Devopal/static/js/main.927916d9.chunk.js"
+  },
+  {
+    "revision": "c8fbd1bd9dc4aec201a0",
+    "url": "/Devopal/static/js/runtime-main.283f4423.js"
+  }
+]);
